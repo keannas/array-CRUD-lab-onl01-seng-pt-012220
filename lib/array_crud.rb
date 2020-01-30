476facc39cfd9d8646dc9dd5a4f2aa1a1a09783d
Keanna Salter <keannasalter91@gmail.com>
2020-01-30 05:47:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  favorite_colors=[red,white,blue,green]
+  favorite_colors=["red","white","blue","green"]
 end
 
 def add_element_to_end_of_array(array, element)
