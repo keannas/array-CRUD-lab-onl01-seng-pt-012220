1dd407b5771ffd5549337cd117e7d8aa2f538b32
Keanna Salter <keannasalter91@gmail.com>
2020-01-30 05:42:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  favorite_colors=[red,white,blue,green]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  favorite_colors << "pink"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  favorite_colors.unshift("purple")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  pink_color = favorite_colors.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  purple =favorite_colors.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  favorite_colors.index("blue")
 end
 
 def retrieve_first_element_from_array(array)
-  
+  favorite_colors[1]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  favorite_colors[3]
 end
